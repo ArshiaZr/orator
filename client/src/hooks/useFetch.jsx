3ae393e2a407dbed92752bc9079a1320b3fd0553
Abdul <lhlrahman@gmail.com>
2024-05-04 13:46:13 -0400

    Remove unused import in page.js component

---
+++ client/src/hooks/useFetch.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (
+  url,
+  method = "GET",
+  bodyData = null,
+  canFetch = true
+) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log("Fetching data from", url);
+      setLoading(true);
+
+      try {
+        let options = {
+          method,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          body: bodyData ? JSON.stringify(bodyData) : null,
+        };
+
+        const response = await fetch(url, options);
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const json = await response.json();
+        setData(json);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    if (canFetch) {
+      fetchData();
+    }
+
+    // Cleanup function to cancel fetch on component unmount
+    return () => {};
+  }, [url, canFetch]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
