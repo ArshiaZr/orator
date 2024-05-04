f501d2ca984ad8035364aca17132f66f157fb063
Abdul <lhlrahman@gmail.com>
2024-05-04 10:30:16 -0400

    Refactor page.js component to use SignupForm and add Lucide Divide icon

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,12 +1,14 @@
 import Upload from "@/components/upload";
 import SignupForm from "@/components/signup";
 import { DrawerModal } from "@/components/feedbackModal";
+import { Divide } from "lucide-react";
 export default function Home() {
-
   const feedback = "dslkf;sldkf";
   return (
-    <DrawerModal
-    feedback={feedback}
-    />
+    <div className="w-screen h-screen">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <SignupForm/>
+      </div>
+    </div>
   );
 }
