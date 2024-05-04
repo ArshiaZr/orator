1c71719bfc69c5eb416974f98cdfa6d1a5951aa6
Abdul <lhlrahman@gmail.com>
2024-05-04 10:38:10 -0400

    multi step loader

---
+++ client/src/components/MutliStepLoader.jsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useState } from "react";
+import { MultiStepLoader as Loader } from "./ui/multi-step-loader";
+
+export default function MultiStepLoader({
+  loadingStates,
+  currentState,
+  setCurrentState,
+  loading,
+  setLoading,
+}) {
+  return (
+    <div className="w-full h-[60vh] flex items-center justify-center">
+      {/* Core Loader Modal */}
+      <Loader
+        loadingStates={loadingStates}
+        loading={loading}
+        currentState={currentState}
+        setCurrentState={setCurrentState}
+      />
+
+      {/* {loading && (
+        <button
+          className="fixed top-4 right-4 text-white z-[120]"
+          onClick={() => setLoading(false)}
+        >
+          <TbSquareRoundedX className="h-10 w-10" />
+        </button>
+      )} */}
+    </div>
+  );
+}
