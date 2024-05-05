cd5801749e42da972aaaa6bfb90fb93bb0c226fa
Abdul <lhlrahman@gmail.com>
2024-05-05 00:52:20 -0400

    done dashboard

---
+++ client/src/components/cards.tsx
@@ -0,0 +1,58 @@
+"use client"
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./bento-grid";
+import {
+  IconClipboardCopy,
+  IconFileBroken,
+  IconSignature,
+} from "@tabler/icons-react";
+import Image from "next/image";
+
+export default function Bento() {
+    return (
+        <BentoGrid>
+            {items.map((item, i) => (
+                <BentoGridItem
+                    key={i}
+                    title={item.title}
+                    description={item.description || "teslwjf;kljsd;lfjk;sdlkf;"}
+                    header={item.header}
+                    icon={item.icon}
+                    className={i === 4 || i === 6 ? "md:col-span-2" : ""}
+                />
+            ))}
+        </BentoGrid>
+    );
+}
+const Skeleton = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100">
+    <Image src={"./vertex.svg"} height={200} width={200} alt={""} className="rounded-2xl" />
+
+  </div>
+);
+const items = [
+  {
+    title: "The Dawn of Innovation",
+    description: "Explore the birth of groundbreaking ideas and inventions.",
+    header: <Skeleton />,
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Digital Revolution",
+    description: "Dive into the transformative power of technology.",
+    header: <Skeleton />,
+    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Art of Design",
+    description: "Discover the beauty of thoughtful and functional design.",
+    header: <Skeleton />,
+    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Art of Design",
+    description: "Discover the beauty of thoughtful and functional design.",
+    header: <Skeleton />,
+    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+  }
+];
